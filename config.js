492e3b1c41fa14e827159b54a1972c746ee99eed
nawafjawad141-cmyk <nawafjawad141@gmail.com>
2026-04-13 15:48:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 Config({
     "network-name": "باب المندب",
     "service-number": "773093894",
-    "login-type": "User",
-    "news-line": "",
-    "input-type": "",
+    "login-type": "both",
+    "news-line": "مرحباً بكم في شبكة باب المندب اللاسلكية - نتمنى لكم خدمة ممتعة",
+    "input-type": "text",
     "input-autocomplete": "off",
     "input-rm-white-spaces": 1,
     "input-to-lower": 0,
@@ -23,7 +23,15 @@ Config({
     "try-count": 5,
     "redirect-to-esterahah": "",
     "redirect-to-mobasher": "",
-    "show-date-field": false,
-    "profiles": [],
-    "sell-points": []
+    "show-date-field": true,
+    "profiles": [
+        {"price": "200 ريال", "time": "6 ساعات", "transfer": "500 ميجا بايت", "validity": "يومين"},
+        {"price": "300 ريال", "time": "12 ساعات", "transfer": "800 ميجا بايت", "validity": "3 أيام"},
+        {"price": "600 ريال", "time": "مفتوح", "transfer": "1600 ميجا بايت", "validity": "7 أيام"}
+    ],
+    "sell-points": [
+        {"name": "المركز الرئيسي - حي السلام"},
+        {"name": "فرع المعلاء"},
+        {"name": "جميع نقاط البيع المعتمدة"}
+    ]
 })
